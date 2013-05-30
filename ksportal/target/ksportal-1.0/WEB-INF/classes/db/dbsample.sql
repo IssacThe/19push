d57f14c8c81c36c3efe557e52d105a16cfe3c7cb
Fadel TOURE <ftoure@nightingalemd.com>
2013-05-30 19:03:28 -0400

    remove target

--- ksportal/target/ksportal-1.0/WEB-INF/classes/db/dbsample.sql
+++
@@ -1,5 +0,0 @@
---liquibase formatted sql
-
---changeset FadelTOURE:(dbms:postgresql failOnError:true)
-INSERT INTO student ( accountid , studentid, studentfirstname,studentlastname, createddate)
-VALUES(1,nextval('student_seq'),'fadel','toure',now());
